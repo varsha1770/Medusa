88ea3931c1bf4b1049f6e091d65eb8bfac7bdac7
varsha janapati <janapativarsha6@gmail.com>
2026-04-09 21:33:06 +0530

    Fix: Resolved infinite redirect loop for default region when backend is offline

--- tif-medusa-storefront/src/middleware.ts
+++ tif-medusa-storefront/src/middleware.ts
@@ -111,7 +111,8 @@ export async function middleware(request: NextRequest) {
   const countryCode = regionMap && (await getCountryCode(request, regionMap))
 
   const urlHasCountryCode =
-    countryCode && request.nextUrl.pathname.split("/")[1].includes(countryCode)
+    (countryCode && request.nextUrl.pathname.split("/")[1].includes(countryCode)) ||
+    (request.nextUrl.pathname.split("/")[1] === DEFAULT_REGION)
 
   // if one of the country codes is in the url and the cache id is set, return next
   if (urlHasCountryCode && cacheIdCookie) {
